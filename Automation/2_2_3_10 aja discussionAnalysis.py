81e0ec9ebaf62f48739db75640046b6960cdf731
lauri <laurkang@edu.lapinamk.fi>
2024-12-15 19:14:05 +0200

    Viimeistelyä ja säätöä

--- Automation/2_2_3_10 aja discussionAnalysis.py
+++ Automation/2_2_3_10 aja discussionAnalysis.py
@@ -16,11 +16,13 @@ Aja discussionAnalysis.py osallistujalistalla
     File Should Exist    ${TARGET_FOLDER}/${PARTICIPANT_LIST}
 
     # Vaihe 2: Suorita Python-skripti ja anna parametrit
-    Run Process    python    ${DISCUSSION_SCRIPT}    ${TARGET_FOLDER}    ${PARTICIPANT_LIST}    cwd=${TARGET_FOLDER}
-    ${output}=    Get Process Output
+    ${result}    Run Process    python    ${TARGET_FOLDER}/${DISCUSSION_SCRIPT}    ${TARGET_FOLDER}    ${PARTICIPANT_LIST}    cwd=${TARGET_FOLDER}    shell=True
 
-    # Vaihe 3: Lokita tulosteet ja varmista suorituksen onnistuminen
+    # Vaihe 3: Hae tulosteet ja tarkista ne
+    ${output}    Get Process Output    ${result}
     Log    ${output}
+
+    # Vaihe 4: Varmista suorituksen onnistuminen
     Should Contain    ${output}    Program Name:
     Should Contain    ${output}    Files in the target folder:
     Should Contain    ${output}    All Discussions:
